30e0b9600005a828573dd51382e77a4d2289e22e
mike.romano <michael.romano4@scranton.edu>
2026-05-11 21:34:02 -0400

    Added a fake RecipeRepository for testing VMs. Added basic tests for RecipeViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     ksp("androidx.room:room-compiler:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
 
+    //needed for testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
     //Added for navigation support
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
---
+++ app/src/test/java/com/miomi/recipe/viewmodel/RecipeViewModelTest.kt
@@ -0,0 +1,121 @@
+package com.miomi.recipe.viewmodel
+
+import com.miomi.recipe.FakeRecipeRepository
+import com.miomi.recipe.model.Recipe
+import com.miomi.recipe.util.MainDispatcherRule
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RecipeViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var fakeRecipeRepository: FakeRecipeRepository
+    private lateinit var viewModel: RecipeViewModel
+
+    @Before
+    fun setup(){
+        fakeRecipeRepository = FakeRecipeRepository()
+        viewModel = RecipeViewModel(fakeRecipeRepository)
+    }
+
+    @Test
+    fun toggleFavoriteTest(){
+        fakeRecipeRepository.seedRecipes(
+            Recipe(recipeId = 1, name = "Pasta", category = "Dinner", isFavorite = true)
+        )
+        viewModel.toggleFavorite(recipeId = 1, isFavorite = true)
+
+        val updated = fakeRecipeRepository.recipes.first {it.recipeId == 1}
+
+        assertFalse(updated.isFavorite)
+    }
+
+    @Test
+    fun addRecipeTest(){
+        val formState = AddRecipeFormState(
+            recipeName = "Pasta",
+            category = "Dinner",
+            ingredients = listOf(
+                IngredientEntry(name = "Spaghetti", quantity = "2", unit = "cups"),
+                IngredientEntry(name = "Sauce", quantity = "1", unit = "jar")
+            ),
+            steps = listOf(
+                StepEntry(description = "Boil water"),
+                StepEntry(description = "Cook noodles"),
+                StepEntry(description = "Add sauce")
+            )
+        )
+
+        viewModel.addRecipe(formState)
+
+        //TODO: should all of these be individual test cases?
+        assertEquals(1, fakeRecipeRepository.recipes.size)
+        assertEquals("Pasta", fakeRecipeRepository.recipes[0].name)
+
+
+        assertEquals(2, fakeRecipeRepository.ingredients.size)
+        assertTrue(fakeRecipeRepository.ingredients.all { it.recipeId == fakeRecipeRepository.recipes[0].recipeId })
+
+        assertEquals(3, fakeRecipeRepository.steps.size)
+        assertEquals(1, fakeRecipeRepository.steps[0].sequenceNum)
+        assertEquals(2, fakeRecipeRepository.steps[1].sequenceNum)
+        assertEquals(3, fakeRecipeRepository.steps[2].sequenceNum)
+    }
+
+
+    @Test
+    fun updateRecipe(){
+
+        fakeRecipeRepository.seedRecipes(
+            Recipe(recipeId = 1, name = "Old spaghetti", "Dinner", isFavorite = false)
+        )
+
+        val formState = AddRecipeFormState(
+            recipeId = 1,
+            recipeName = "New Pasta",
+            category = "Dinner",
+            isFavorite = false,
+            ingredients = listOf(
+                IngredientEntry(name = "Noodles", quantity = "2", unit = "cups")
+            ),
+            steps = listOf(
+                StepEntry(description = "Boil water"),
+                StepEntry(description = "Cook noodles")
+            )
+        )
+
+        viewModel.updateRecipe(formState)
+
+        assertEquals("New Pasta", fakeRecipeRepository.recipes.first { it.recipeId == 1 }.name)
+
+        assertEquals(1, fakeRecipeRepository.ingredients.size)
+        assertEquals("Noodles", fakeRecipeRepository.ingredients[0].name)
+
+        assertEquals(2, fakeRecipeRepository.steps.size)
+        assertEquals(1, fakeRecipeRepository.steps[0].sequenceNum)
+        assertEquals(2, fakeRecipeRepository.steps[1].sequenceNum)
+    }
+
+
+    @Test
+    fun updateRecipeWithNullRecipeId() {
+        val formState = AddRecipeFormState(
+            recipeId = null,  //this should trigger an early return and nothing should be updated
+            recipeName = "Ghost Recipe",
+            category = "Dinner"
+        )
+
+        viewModel.updateRecipe(formState)
+
+        // everything should still be empty
+        assertTrue(fakeRecipeRepository.recipes.isEmpty())
+        assertTrue(fakeRecipeRepository.ingredients.isEmpty())
+        assertTrue(fakeRecipeRepository.steps.isEmpty())
+    }
+}
